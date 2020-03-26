baa522aaf80fef87a322d623e8f82da30e16e963
guoxudong2019 <guoxudong0115@gmail.com>
2020-03-26 19:14:16 +0800

    新增开启线程功能

--- Concurrency/main.cpp
+++ Concurrency/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <thread>
+#include <atomic>
+#include <vector>
+#include <mutex>
+
+using namespace std;
+
+atomic<int> g_at = 0;
+mutex g_mutex;
+
+void output() {
+	lock_guard<mutex> mu(g_mutex);
+	cout << "Thread id :" << g_at++ << endl;
+}
 
 int main() {
+	vector<thread> ths;
+	for (int i = 0; i < 10; i++) {
+		ths.push_back(thread(output));
+	}
 
+	for (auto &item : ths) {
+		item.join();
+	}
 }
