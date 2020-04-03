cc8461a3bc3cf8cd76699ec561dc54d5b0161435
guoxudong2019 <guoxudong0115@gmail.com>
2020-04-03 21:17:27 +0800

    添加Unique_ptr

--- Memory/UniquePtr.h
+++ Memory/UniquePtr.h
@@ -1,5 +1,57 @@
 #pragma once
+
+template<typename T>
 class UniquePtr
 {
+public:
+	UniquePtr(T *ptr = nullptr) :
+		m_ptr(ptr) {
+
+	}
+
+	~UniquePtr(){
+		if (m_ptr) {
+			delete m_ptr;
+			m_ptr = nullptr;
+		}
+	}
+
+	UniquePtr(const UniquePtr& ptr) = delete;
+	UniquePtr& operator=(const UniquePtr& ptr) = delete;
+
+	UniquePtr(const UniquePtr&& ptr) noexcept : m_ptr(ptr.m_ptr) {
+		ptr.m_ptr = nullptr;
+	}
+
+	UniquePtr& operator=(const T&& ptr) noexcept {
+		m_ptr = ptr.m_ptr;
+		ptr.m_ptr = nullptr;
+	}
+
+	void reset(T *ptr) {
+		if (ptr != m_ptr)
+			m_ptr = ptr;
+	}
+
+	T* release() {
+		T* temp = m_ptr;
+		delete m_ptr;
+		return temp;
+	}
+
+	T* get() {
+		return m_ptr;
+	}
+
+	T &operator*() {
+		return *m_ptr;
+	}
+
+	T *operator->() {
+		return &*(*this);
+	}
+
+private:
+	T*	m_ptr;
 };
 
