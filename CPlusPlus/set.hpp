423fd5272acddc770c460484317a2d9eaf5d7955
guoxudong <510105194@qq.com>
2020-06-16 23:19:22 +0800

    no message

--- CPlusPlus/set.hpp
+++ CPlusPlus/set.hpp
@@ -1,5 +1,7 @@
 #include <set>
+#include <vector>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -16,19 +18,37 @@ public:
 	int	m_num {0};
 };
 
+bool lessFunction(string* left, string* right) {
+	return *left < *right;
+};
+
 void testSet()
 {
-	set<MySetItem> myset;
-	
-	for (int i = 10; i > 0; i--) {
+	/*set<MySetItem> myset;
+
+	for (int i = 100; i > 0; i--) {
 		myset.insert(MySetItem(i));
-	}
+	}*/
 
 	/*for (auto itr = myset.begin(); itr != myset.end(); itr++) {
 		cout << (*itr).m_num << endl;
 	}*/
 
-	for (auto &item : myset) {
+	/*for (auto &item : myset) {
 		cout << item.m_num << endl;
+	}*/
+
+	auto lessFunctionLambda = [](string* left, string* right) {
+		return *left < *right;
+	};
+
+	set<string*, decltype(lessFunction)> myStrSet;
+	myStrSet.insert(new string("ahorse"));
+	myStrSet.insert(new string("bmonkey"));
+	myStrSet.insert(new string("cpig"));
+	myStrSet.insert(new string("dfish"));
+
+	for (auto &item : myStrSet) {
+		cout << *item << endl;
 	}
 }
